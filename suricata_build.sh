75cd43b1c9fdc1698e3e92fc9830a6578f9442e2
Nick Price <nick@spun.io>
2021-05-30 15:27:02 -0700

    a

--- suricata_build.sh
+++ suricata_build.sh
@@ -2,3 +2,17 @@
 sudo apt install libpcre3-dev libhyperscan-dev libyaml-dev libjansson-dev libpcap-dev libz-dev libnss3-dev libbpf-dev libnet-dev libmagic-dev liblz4-dev libcap-ng-dev libmaxminddb-dev
 
 ./configure --enable-luajit --enable-ebpf --enable-geoip
+
+sudo make -j32
+sudo make install-full
+sudo cp etc/suricata.logrotate /etc/logrotate.d/suricata.logrotate
+sudo cp etc/suricata.service /etc/systemd/system/suricata.service
+sudo systemctl daemon-reload
+sudo systemctl enable suricata
+sudo suricata-update update-sources
+sudo suricata-update enable-source ptresearch/attackdetection
+sudo suricata-update enable-source sslbl/ssl-fp-blacklist
+sudo suricata-update enable-source sslbl/ja3-fingerprints
+sudo suricata-update enable-source etnetera/aggressive
+sudo suricata-update enable-source tgreen/hunting
+sudo suricata-update update --ignore \*events.rules
