3c2d123737dad75f98f9c926e53a387e18cb9dbc
Nick Price <nick@spun.io>
2023-04-30 11:04:21 -0700

    rm old stuff

--- mac.sh
+++
@@ -1,4 +0,0 @@
-defaults write -g com.apple.desktopservices DSDontWriteNetworkStores true
-defaults write -g ApplePressAndHoldEnabled 0
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
